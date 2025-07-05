835d3a661615008c28ef4c1937261b81315666c4
Kartik Sehra <sehrakartik73@gmail.com>
2025-07-05 14:39:29 +0530

    Update email_function.py

--- email_function.py
+++ email_function.py
@@ -1,32 +1,32 @@
-from datetime import datetime
-import pandas as pd
-import smtplib
-from email.message import EmailMessage
+from datetime import datetime #imports the current date and time
+import pandas as pd    #importing pandas to read the csv file
+import smtplib #python lib to send mail via python
+from email.message import EmailMessage #creates the content of mail to be sent
 
 def send_bday_emails():
-    EMAIL = "your_email@gmail.com"
-    APP_PASSWORD = "your_app_password"
+    EMAIL = "your_email@gmail.com" #USER email
+    APP_PASSWORD = "your_app_password" #16 character app password
 
     today = datetime.now().strftime("%m-%d")
     current_year = datetime.now().strftime("%Y")
 
     # Read the birthday list
-    birthdays = pd.read_csv("birthday.csv")
+    birthdays = pd.read_csv("birthday.csv") #reads the csv file
 
     # Read log if exists
     try:
         with open("sent_log.txt", "r") as f:
-            sent_log = f.read()
+            sent_log = f.read()#reads the latest mail log
     except FileNotFoundError:
-        sent_log = ""
+        sent_log = ""#if there is no mail log
 
     for _, row in birthdays.iterrows():
         bday = datetime.strptime(row["birthday"], "%Y-%m-%d").strftime("%m-%d")
         recipient = row["email"]
         name = row["name"]
-        log_id = f"{current_year}-{today}-{recipient}"
+        log_id = f"{current_year}-{today}-{recipient}"#log id to make sure the mail is not repeated 
 
-        if bday == today and log_id not in sent_log:
+        if bday == today and log_id not in sent_log:#if the mail id is not in the csv file then that means the is going to be sent for the first time
             try:
                 message = EmailMessage()
                 message["Subject"] = "🎉 Happy Birthday!"
