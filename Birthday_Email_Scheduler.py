f6ea8ae89973b3ad161a9a1b23104235577f67a4
Kartik Sehra <sehrakartik73@gmail.com>
2025-07-05 14:34:27 +0530

    Update Birthday_Email_Scheduler.py

--- Birthday_Email_Scheduler.py
+++ Birthday_Email_Scheduler.py
@@ -6,8 +6,8 @@ from email_function import send_bday_emails
 def work():
     send_bday_emails()
 
-schedule.every().day.at("08:00").do(work)
-# schedule.every(1).minutes.do(work) #if want to send test emails every 1 minute
+schedule.every(60).minutes.do(send_bday_emails)
+# schedule.every(1).minutes.do(work) 
 
 
 while True:
