05b8fc53629c07d2e7de426bbd76147a48164ef3
Kartik Sehra <sehrakartik73@gmail.com>
2025-07-05 14:40:15 +0530

    Update Birthday_Email_Scheduler.py

--- Birthday_Email_Scheduler.py
+++ Birthday_Email_Scheduler.py
@@ -3,10 +3,10 @@ import time
 
 from email_function import send_bday_emails
 
-def work():
-    send_bday_emails()
+# def work():
+#     send_bday_emails()
 
-schedule.every(60).minutes.do(send_bday_emails)
+schedule.every(60).minutes.do(send_bday_emails)#runs the function every 60 minutes from the computer start up
 # schedule.every(1).minutes.do(work) 
 
 
